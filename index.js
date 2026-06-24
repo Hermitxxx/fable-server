17cd0a64c6d8335203089452b50fbf32672da474
Hermitxxx <cvenom477@gmail.com>
2026-06-24 23:17:43 +0600

    added new apis for getting book stats of a writer

--- index.js
+++ index.js
@@ -98,6 +98,122 @@ async function run() {
             res.json(result)
         })
 
+        // get writer's stats
+        app.get('/api/writer-stats', async (req, res) => {
+            const query = {}
+            if (req.query.writerId) {
+                query.writerId = req.query.writerId
+            }
+
+            console.log(query);
+
+            const books = await booksColl.find({
+                writerId: query.writerId
+            }).toArray()
+
+            const stats = await booksColl.aggregate([
+                {
+                    $match: {
+                        writerId: query.writerId
+                    }
+                },
+                {
+                    $group: {
+                        _id: null,
+
+                        totalBooks: {
+                            $sum: 1
+                        },
+
+                        publishedBooks: {
+                            $sum: {
+                                $cond: [
+                                    {
+                                        $eq: [
+                                            "$parchment",
+                                            "published"
+                                        ]
+                                    },
+                                    1,
+                                    0
+                                ]
+                            }
+                        },
+
+                        unpublishedBooks: {
+                            $sum: {
+                                $cond: [
+                                    {
+                                        $eq: [
+                                            "$parchment",
+                                            "unpublished"
+                                        ]
+                                    },
+                                    1,
+                                    0
+                                ]
+                            }
+                        },
+
+                        totalSales: {
+                            $sum: "$purchaseCount"
+                        }
+                    }
+                }
+            ]).toArray()
+
+            res.json({
+                stats: stats[0],
+                books
+            })
+        })
+
+        // publish a book (writer fucniton)
+        app.post('/api/books', async (req, res) => {
+            const data = req.body
+            const result = await booksColl.insertOne(data)
+            res.json(result)
+        })
+
+        // publish or unpublish a book (admin function)
+        app.patch('/api/books', async (req, res) => {
+            const query = {}
+            if (req.body.bookId) {
+                query.bookId = req.body.bookId
+            }
+
+            if (req.body.parchment) {
+                query.parchment = req.body.parchment
+            }
+
+            const filter = {
+                _id: new ObjectId(query.bookId)
+            }
+
+            const updatedData = {
+                $set: {
+                    parchment: query.parchment
+                }
+            }
+
+            const result = await booksColl.updateOne(filter, updatedData)
+            res.json(result)
+        })
+
+        // delete a book
+        app.delete('/api/books/:id', async (req, res) => {
+            const id = req.params.id
+
+            console.log(id);
+            const query = {
+                _id: new ObjectId(id)
+            }
+
+            const result = await booksColl.deleteOne(query)
+            res.json(result)
+        })
+
+        // users
         // get all users 
         app.get('/api/users', async (req, res) => {
             const cursor = usersColl.find()
@@ -131,6 +247,19 @@ async function run() {
 
             const result = await usersColl.updateOne(filter, updatedData)
 
+            res.json(result)
+        })
+
+        // delete a user
+        app.delete('/api/users/:id', async (req, res) => {
+            const id = req.params.id
+
+            console.log(id);
+            const query = {
+                _id: new ObjectId(id)
+            }
+
+            const result = await usersColl.deleteOne(query)
             res.json(result)
         })
 
